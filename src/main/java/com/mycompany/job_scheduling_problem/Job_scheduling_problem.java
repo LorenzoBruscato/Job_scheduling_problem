1ac0a488654b8a6f6079fb9faf68e9528325910c
LorenzoBruscato <lorenzobruscato6@gmail.com>
2026-04-05 18:51:38 -0300

    Initial commit projeto

---
+++ src/main/java/com/mycompany/job_scheduling_problem/Job_scheduling_problem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.job_scheduling_problem;
+
+/**
+ *
+ * @author Lorenzo Bruscato
+ */
+public class Job_scheduling_problem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
